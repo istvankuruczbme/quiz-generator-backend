c4058f1ad900e1a99480615fd0308b69603429a5
István Kurucz <istvan.kurucz@edu.bme.hu>
2025-04-05 12:57:28 +0200

    Database v8

--- src/drizzle/schema/questionPoints.ts
+++ src/drizzle/schema/questionPoints.ts
@@ -1,18 +1,23 @@
-import { pgTable, real, uuid } from "drizzle-orm/pg-core";
+import { pgTable, real, uniqueIndex, uuid } from "drizzle-orm/pg-core";
 import { QuestionTable } from "./question";
 import { relations } from "drizzle-orm";
 import { id } from "../schemaHelpers";
 
 // Schema
-export const QuestionPointsTable = pgTable("question_points", {
-	id,
-	correct: real("correct").notNull(),
-	wrong: real("wrong").notNull(),
-	empty: real("empty").notNull(),
-	questionId: uuid("question_id")
-		.references(() => QuestionTable.id, { onDelete: "cascade" })
-		.notNull(),
-});
+export const QuestionPointsTable = pgTable(
+	"question_points",
+	{
+		id,
+		correct: real("correct").notNull(),
+		wrong: real("wrong").notNull(),
+		empty: real("empty").notNull(),
+		questionId: uuid("question_id")
+			.unique()
+			.references(() => QuestionTable.id, { onDelete: "cascade" })
+			.notNull(),
+	},
+	(table) => [uniqueIndex("question_points_question_id_index").on(table.questionId)]
+);
 
 // Relations
 export const questionPointsRelations = relations(QuestionPointsTable, ({ one }) => {
--- src/drizzle/schema/quizConfig.ts
+++ src/drizzle/schema/quizConfig.ts
@@ -1,4 +1,4 @@
-import { pgEnum, pgTable, uuid } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable, uniqueIndex, uuid } from "drizzle-orm/pg-core";
 import { QuizTable } from "./quiz";
 import { relations } from "drizzle-orm";
 import { id } from "../schemaHelpers";
@@ -25,15 +25,20 @@ export const quizConfigQuestionOrderEnum = pgEnum(
 export type QuizConfigQuestionOrder = (typeof quizConfigQuestionOrderOptions)[number];
 
 // Schema
-export const QuizConfigTable = pgTable("quiz_config", {
-	id,
-	state: quizConfigStateEnum("state").default("DRAFT").notNull(),
-	visibility: quizConfigVisibilityEnum("visibility").default("PUBLIC").notNull(),
-	questionOrder: quizConfigQuestionOrderEnum("question_order").default("NORMAL").notNull(),
-	quizId: uuid("quiz_id")
-		.references(() => QuizTable.id, { onDelete: "cascade" })
-		.notNull(),
-});
+export const QuizConfigTable = pgTable(
+	"quiz_config",
+	{
+		id,
+		state: quizConfigStateEnum("state").default("DRAFT").notNull(),
+		visibility: quizConfigVisibilityEnum("visibility").default("PUBLIC").notNull(),
+		questionOrder: quizConfigQuestionOrderEnum("question_order").default("NORMAL").notNull(),
+		quizId: uuid("quiz_id")
+			.unique()
+			.references(() => QuizTable.id, { onDelete: "cascade" })
+			.notNull(),
+	},
+	(table) => [uniqueIndex("quiz_config_quiz_id_index").on(table.quizId)]
+);
 
 // Relations
 export const QuizConfigRelations = relations(QuizConfigTable, ({ one }) => {
